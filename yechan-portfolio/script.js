ad805214106c54634d18104a5e03416988cb06df
Walker4990 <bora0193@naver.com>
2025-09-10 16:06:45 +0900

    feat : 모바일 사이드바 수정

--- yechan-portfolio/script.js
+++ yechan-portfolio/script.js
@@ -75,3 +75,7 @@ document.querySelectorAll(".sidebar button").forEach((btn) => {
     }
   });
 });
+function toggleMenu() {
+  const menu = document.querySelector(".mobile-menu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
